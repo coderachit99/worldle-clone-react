a6519e14347353f6eb942fa323aae76ee7e29bec
rachit <rachitbhusal@gmail.com>
2022-06-26 15:46:23 +0545

    keyup added :key:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Wordle from './components/Wordle';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [solution, setSolution] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const api = await fetch('http://localhost:3001/solutions');
+            const json = await api.json();
+            const randomSolution = json[Math.floor(Math.random() * json.length)]; // random int between 0 and 14
+
+            setSolution(randomSolution.word);
+        };
+        fetchData();
+    }, [setSolution]);
+
+    return (
+        <div className='App'>
+            <h1>Wordle (Lingo)</h1>
+            {solution && <Wordle solution={solution} />}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+const useWordle = (solution) => {
+    const [turn, setTurn] = useState(0);
+    const [currentGuess, setCurrentGuess] = useState('');
+    const [guesses, setGuesses] = useState([]);
+    const [history, setHistory] = useState([]);
+    const [isCorrect, setIsCorrect] = useState(false);
+
+    const formatGuess = () => {};
+
+    const addNewGuess = () => {};
+
+    const handleKeyUp = ({ key }) => {
+        console.log(key);
+    };
+
+    return { turn, currentGuess, guesses, isCorrect, handleKeyUp };
+};
+
+export default useWordle;
